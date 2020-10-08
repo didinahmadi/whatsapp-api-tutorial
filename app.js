228bdfecdfe11acdbbb898c22104de53a8608a42
Nur Muhammad <blog.nurmuhammad@gmail.com>
2020-10-08 11:51:55 +0800

    Initial commit: tutorial part 1 completed

---
+++ app.js
@@ -0,0 +1,40 @@
+const { Client } = require('whatsapp-web.js');
+const qrcode = require('qrcode-terminal');
+const fs = require('fs');
+
+const SESSION_FILE_PATH = './whatsapp-session.json';
+let sessionCfg;
+if (fs.existsSync(SESSION_FILE_PATH)) {
+    sessionCfg = require(SESSION_FILE_PATH);
+}
+
+const client = new Client({ puppeteer: { headless: true }, session: sessionCfg });
+
+client.on('qr', (qr) => {
+    console.log('QR RECEIVED', qr);
+    qrcode.generate(qr);
+});
+
+client.on('authenticated', (session) => {
+    console.log('AUTHENTICATED', session);
+    sessionCfg=session;
+    fs.writeFile(SESSION_FILE_PATH, JSON.stringify(session), function (err) {
+        if (err) {
+            console.error(err);
+        }
+    });
+});
+
+client.on('ready', () => {
+    console.log('Client is ready!');
+});
+
+client.on('message', msg => {
+    if (msg.body == '!ping') {
+        msg.reply('pong');
+    } else if (msg.body == 'good morning') {
+      msg.reply('selamat pagi');
+    }
+});
+
+client.initialize();
---
+++ readme.md
@@ -0,0 +1,7 @@
+# Whatsapp API Tutorial
+
+Hi, this is the implementation example of whatsapp-web.js
+
+Watch the tutorials:
+- <a href="#">Whatsapp API Tutorial: Part 1</a>
+- <a href="#">Whatsapp API Tutorial: Part 2 (coming soon)</a>
